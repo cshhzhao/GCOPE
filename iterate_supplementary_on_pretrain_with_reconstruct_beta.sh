186712ac069706e8155b5ff6bc95b6d18dc957a8
cshhzhao <zhaohaihong_cn@outlook.com>
2024-02-07 17:57:31 +0800

    update core files

--- iterate_supplementary_on_pretrain_with_reconstruct_beta.sh
+++ iterate_supplementary_on_pretrain_with_reconstruct_beta.sh
@@ -1,14 +1,10 @@
+#!/bin/bash
 
-learning_rates=(1e-2 1e-3 5e-3 1e-4 5e-4)
 target_datasets=("cora")
 
 backbone="fagcn"
 backbone_tuning=1
 split_method="RandomWalk"
-
-few_shot=1
-batch_sizes=(100) 
-
 
 for target_dataset in "${target_datasets[@]}"; do
     source_dataset_str=""
@@ -26,12 +22,4 @@ for target_dataset in "${target_datasets[@]}"; do
 
     python src/exec.py --config-file pretrain.json --general.save_dir "storage/reconstruct" --general.reconstruct 0.2 --data.name "${source_dataset_str}" --pretrain.split_method ${split_method}
     
-    for lr in "${learning_rates[@]}"
-    do
-        # 对每个 batch_size
-        for bs in "${batch_sizes[@]}"
-        do
-            python src/exec.py --general.func adapt  --general.save_dir "storage/balanced_few_shot_fine_tune_backbone_with_rec" --general.few_shot $few_shot  --general.reconstruct 0.0 --data.node_feature_dim 100 --data.name $target_dataset --adapt.method finetune --data.supervised.ratios 0.1,0.1,0.8 --model.backbone.model_type ${backbone} --model.saliency.model_type "none" --adapt.method finetune --adapt.pretrained_file "storage/reconstruct/${source_dataset_str}_pretrained_model.pt" --adapt.finetune.learning_rate $lr --adapt.batch_size $bs --adapt.finetune.backbone_tuning $backbone_tuning
-        done 
-    done
 done
